70f7ce41ee382ac993f109097c1819bb14b01324
Claire <claire.tischuk@ufl.edu>
2021-12-29 16:28:24 -0500

    added a second level window with scroll bar and text widget to display definition

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,93 @@
 import requests
 from bs4 import BeautifulSoup
-
-class Word:
-    def __init__(self):
-        pass
         
 
-#class SearchBot:
-    #def __init__()
+class SearchBot:
+    def __init__(self, query):
+        self.query = query
+        self.grammar_dict = {}
+        
+    def search_query(self):
+        URL = "https://www.spanishdict.com/translate/" + self.query
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        
+        green_text = soup.find_all(class_=["_2VBIE8Jw", "_2-ylzxko", "_1vjZoH4D", "_3nHbP_Tm", "_3AjVTHH0", "_2vd6M2gR"])
+        
+        lru_grammar_word = ""
+        lru_green_text = ""
+        definition = ""
+        sentence_pair = []
+
+        for element in green_text:
+            # we are adding in the grammar words 1st tier
+            if element['class'][0] == "_2VBIE8Jw" or element['class'][0] == "_3AjVTHH0":
+                if element.text not in self.grammar_dict:
+                    # this dictionary will have the general uses as the key (text in ()), with values being an tuple of tuples (eng - es translation)
+                    self.grammar_dict[element.text] = {}
+                lru_grammar_word = element.text
+            # general use (text in ())
+            elif element['class'][0] == "_2-ylzxko":
+                # assuming this general use text is not used more than once
+                if element.text not in self.grammar_dict[lru_grammar_word]:
+                    self.grammar_dict[lru_grammar_word][element.text] = {}
+                lru_green_text = element.text
+            elif element['class'][0] =="_2vd6M2gR":
+                #definition = definition + " | " + element.text
+                if definition != "":
+                    definition += (" | " + element.text)
+                else:
+                    definition += (" " + element.text)
+            elif element['class'][0] =="_1vjZoH4D":
+                sentence_pair.append(element.text)
+            elif element['class'][0] =="_3nHbP_Tm":
+                sentence_pair.append(element.text)
+            
+            
+            if len(sentence_pair) == 2:
+                self.grammar_dict[lru_grammar_word][lru_green_text][definition] = (sentence_pair[0], sentence_pair[1])
+                definition = ""
+                sentence_pair.clear()
+                
+    def display_info(self):
+        if self.grammar_dict: 
+            for grammar, meanings in self.grammar_dict.items():
+                print(grammar)
+                for meaning, definitions in meanings.items():
+                    print("\t" + meaning)
+                    for definition, sentences in definitions.items():
+                        print("\t\t" + definition)
+                        print("\t\t\t" + str(sentences))
+                        
+                            
+                print()
+        else:
+            print("must perform search_query method first.")
+            
+    def output_message(self):
+        message = ''
+        if self.grammar_dict: 
+            for grammar, meanings in self.grammar_dict.items():
+                message += ("\t" + grammar + "\n")
+                for meaning, definitions in meanings.items():
+                    message += ("\t\t" + meaning + "\n")
+                    for definition, sentences in definitions.items():
+                        message += ("\t\t\t" + definition + "\n")
+                        message += ("\t\t\t\t" + str(sentences[0]) + "\n")
+                        message += ("\t\t\t\t" + str(sentences[1]) + "\n")
+                        
+                            
+                message += "\n"
+        else:
+            message = "must perform search_query method first."
+            
+        return message
+
+
+
+
+
+"""        
 # function to get request from website
 # TESTING - cat, el gato, el monóculo, el monóculo
 query = "aguantar"
@@ -107,7 +187,7 @@ for grammar, meanings in grammar_dict.items():
 
 
 
-"""
+
 grammar_words = {} # key is grammar word
 green_text = {} # inside of grammar_words, value of grammar_words' keys
 definiton = "" # key of green_text, keep adding to it until you hit a sentence
@@ -141,3 +221,4 @@ for gram in grams:
                 
     print()
 """
+
--- tkinter_box.py
+++ tkinter_box.py
@@ -1,7 +1,27 @@
 import tkinter as tk
+from dictionary import SearchBot
 
 def send_info():
-    submission = entry.get()
+    submission = str(entry.get())
+    search_bot = SearchBot(submission)
+    search_bot.search_query()
+    search_bot.display_info()
+    
+    newWindow = tk.Toplevel(window)
+    
+    newWindow.title("Word")
+    tk.Label(newWindow, text = "this is a new window").pack()
+    
+    message = search_bot.output_message()
+    text_box = tk.Text(newWindow, height=39, width=120, wrap='word')
+    text_box.pack(padx=20, side=tk.LEFT, expand=True)
+    text_box.insert(tk.END, message)
+    
+    sb = tk.Scrollbar(newWindow)
+    
+    sb.pack(side=tk.RIGHT, fill=tk.BOTH)
+    text_box.config(yscrollcommand=sb.set)
+    sb.config(command=text_box.yview)
     
 window = tk.Tk()
 
@@ -10,9 +30,9 @@ window.configure(background="#F5B31E")
 
 label = tk.Label(text="Type in a Spanish or English word")
 entry = tk.Entry()
-button = tk.Button(text="enter")
+button = tk.Button(text="enter", command=send_info)
 label.grid(row=0, column = 0, columnspan=2, pady = 5)
 entry.grid(row=1, column = 0, pady = 5)
-button.grid(row=1, column = 1, sticky="W", command=send_info)
+button.grid(row=1, column = 1, sticky="W")
 window.mainloop()
 
